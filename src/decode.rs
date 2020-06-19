ae71a6c87548cac3e02dab475251832a1335a25b
samyakahuja <samyak.mcs18.du@gmail.com>
2020-06-20 00:02:34 +0530

    init

---
+++ src/decode.rs
@@ -0,0 +1,7 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn decode_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ src/encode.rs
@@ -0,0 +1,7 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn encode_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
